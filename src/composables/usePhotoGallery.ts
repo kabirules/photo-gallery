b8fccc8d182d036904db85f34a1a67edd7e89147
kabi_rules <javifont@gmail.com>
2020-10-09 18:09:36 +0200

    taking photos & showing them

---
+++ src/composables/usePhotoGallery.ts
@@ -0,0 +1,33 @@
+import { ref, onMounted, watch } from 'vue';
+import { Plugins, CameraResultType, CameraSource, CameraPhoto, 
+Capacitor, FilesystemDirectory } from "@capacitor/core";
+
+export function usePhotoGallery() {
+    const { Camera } = Plugins;
+
+    const photos = ref<Photo[]>([]);
+  
+    const takePhoto = async () => {
+        const cameraPhoto = await Camera.getPhoto({
+            resultType: CameraResultType.Uri,
+            source: CameraSource.Camera,
+            quality: 100
+        });
+        const fileName = new Date().getTime() + '.jpeg';
+        const savedFileImage = {
+            filepath: fileName,
+            webviewPath: cameraPhoto.webPath
+        };
+        photos.value = [savedFileImage, ...photos.value];
+    };
+  
+    return {
+        photos,
+        takePhoto
+    };
+}
+
+export interface Photo {
+    filepath: string;
+    webviewPath?: string;
+}

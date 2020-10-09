29f8a5e08275426675201c2167f8556b4b044bf2
kabi_rules <javifont@gmail.com>
2020-10-09 18:38:31 +0200

    storing photos

--- src/composables/usePhotoGallery.ts
+++ src/composables/usePhotoGallery.ts
@@ -3,23 +3,53 @@ import { Plugins, CameraResultType, CameraSource, CameraPhoto,
 Capacitor, FilesystemDirectory } from "@capacitor/core";
 
 export function usePhotoGallery() {
-    const { Camera } = Plugins;
+    const { Camera, Filesystem } = Plugins;
 
     const photos = ref<Photo[]>([]);
   
+    const convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+        const reader = new FileReader;
+        reader.onerror = reject;
+        reader.onload = () => {
+            resolve(reader.result);
+        };
+        reader.readAsDataURL(blob);
+    });
+
+    const savePicture = async (photo: CameraPhoto, fileName: string): Promise<Photo> => {
+        //let base64Data: string;
+      
+        // Fetch the photo, read as a blob, then convert to base64 format
+        const response = await fetch(photo.webPath!);
+        const blob = await response.blob();
+        const base64Data = await convertBlobToBase64(blob) as string;
+      
+        const savedFile = await Filesystem.writeFile({
+          path: fileName,
+          data: base64Data,
+          directory: FilesystemDirectory.Data
+        });
+      
+        // Use webPath to display the new image instead of base64 since it's 
+        // already loaded into memory
+        return {
+          filepath: fileName,
+          webviewPath: photo.webPath
+        };
+    }
+
     const takePhoto = async () => {
         const cameraPhoto = await Camera.getPhoto({
-            resultType: CameraResultType.Uri,
-            source: CameraSource.Camera,
-            quality: 100
+          resultType: CameraResultType.Uri,
+          source: CameraSource.Camera,
+          quality: 100
         });
+      
         const fileName = new Date().getTime() + '.jpeg';
-        const savedFileImage = {
-            filepath: fileName,
-            webviewPath: cameraPhoto.webPath
-        };
+        const savedFileImage = await savePicture(cameraPhoto, fileName);
+      
         photos.value = [savedFileImage, ...photos.value];
-    };
+    };    
   
     return {
         photos,
